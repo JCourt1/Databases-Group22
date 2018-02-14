5c8a54537c74dbb4d9921a2e3113a83c2e0fcd2f
Mark <markbackhouse@live.co.uk>
2018-02-14 13:39:08 +0000

    Minor additions to searching

--- dbCoursework/dashboard/index.php
+++ dbCoursework/dashboard/index.php
@@ -23,65 +23,64 @@
 
             for($rownumber = 0; $rownumber<$rowcount; $rownumber++){
 
-            $data1 = $querry_result -> fetch();
+                $data1 = $querry_result -> fetch();
 
-            $itemID = $data1['itemID'];
-            $title = $data1['title'];
-            $description = $data1['description'];
-            $photo = $data1['photo'];
-            $date = $data1['endDate'];
-            $startPrice = $data1['startPrice'];
+                $itemID = $data1['itemID'];
+                $title = $data1['title'];
+                $description = $data1['description'];
+                $photo = $data1['photo'];
+                $date = $data1['endDate'];
+                $startPrice = $data1['startPrice'];
 
 
-            $querry_result2 = $conn->query( "SELECT bidAmount, bidDate FROM bids WHERE itemID = ".$data1['itemID']." ORDER BY bidAmount LIMIT 1");
+                $querry_result2 = $conn->query( "SELECT bidAmount, bidDate FROM bids WHERE itemID = ".$data1['itemID']." ORDER BY bidAmount LIMIT 1");
 
-            $data2 = $querry_result2 -> fetch();
+                $data2 = $querry_result2 -> fetch();
 
-            $currentPrice = $data2['bidAmount'];
-            $lastBid = $data2['bidDate'];
+                $currentPrice = $data2['bidAmount'];
+                $lastBid = $data2['bidDate'];
 
 
 
 
             $chaine = '<div class="col-xs-6 col-sm-3 placeholder">
 
+                <!-- Modal -->
+                <div id="myModal'.$rownumber.'" class="modal fade" role="dialog">
+                    <div class="modal-dialog">
 
-  <!-- Modal -->
-  <div id="myModal'.$rownumber.'" class="modal fade" role="dialog">
-<div class="modal-dialog">
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h2 class="modal-title">'.$title.'</h4>
+                            </div>
+                            <div class="modal-body">
+                                <img src="'.$photo.'" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail"
+                                <p>'.$description.'</p>
+                                <h3>Bidding ends: '.$date.' </h2>
+                                <h3> Start Price: '.$startPrice.' </h2>
+                                <h3> Current Price: '.$currentPrice.' </h2>
+                                <h3> Last Bid: '.$lastBid.' </h2>
+                            </div>
+                            <div class="modal-footer">
+                                <div class="form-group pull-left">
+                                    <input type="text" name="bid" id="inputBid" >
+                                </div>
+                                <button type="button" class="btn btn-default pull-left" action="addBid()" >Bid</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
 
-<!-- Modal content-->
-<div class="modal-content">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <h2 class="modal-title">'.$title.'</h4>
-  </div>
-  <div class="modal-body">
-    <img src="'.$photo.'" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail"
-    <p>'.$description.'</p>
-    <h3>Bidding ends: '.$date.' </h2>
-    <h3> Start Price: '.$startPrice.' </h2>
-    <h3> Current Price: '.$currentPrice.' </h2>
-    <h3> Last Bid: '.$lastBid.' </h2>
-  </div>
-  <div class="modal-footer">
-  <div class="form-group pull-left">
-  <input type="text" name="bid" id="inputBid" >
-</div>
-    <button type="button" class="btn btn-default pull-left" action="addBid()" >Bid</button>
-    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-  </div>
-</div>
+                    </div>
+                </div>
 
-</div>
-</div>
-
-              <img src="'.$photo.'" width="200" height="200" class="img" alt="Generic placeholder thumbnail" data-toggle="modal" data-target="#myModal'.$rownumber.'">
-              <a  data-toggle="modal" data-target="#myModal'.$rownumber.'">
-              <h4>' .$title.'
-              </h4>
-              <span class="text-muted">  '.$description.' </span>
-              </a>
+                <img src="'.$photo.'" width="200" height="200" class="img" alt="Generic placeholder thumbnail" data-toggle="modal" data-target="#myModal'.$rownumber.'">
+                <a  data-toggle="modal" data-target="#myModal'.$rownumber.'">
+                    <h4>' .$title.'
+                    </h4>
+                    <span class="text-muted">  '.$description.' </span>
+                </a>
             </div>';
 
             echo $chaine;
@@ -90,11 +89,11 @@
 
 
 
-function addBid(){
-  if(isset($_POST["bid"]) && $currentPrice<$_POST["bid"]){
-    $conn->query("INSERT INTO bids (itemID, buyerID, bidAmount, bidDate) VALUES (".$itemID.",".$buyerID.",".$_POST["bid"].",".date("Y-m-d")." ) ");
-  }
-}
+            function addBid(){
+              if(isset($_POST["bid"]) && $currentPrice<$_POST["bid"]){
+                $conn->query("INSERT INTO bids (itemID, buyerID, bidAmount, bidDate) VALUES (".$itemID.",".$buyerID.",".$_POST["bid"].",".date("Y-m-d")." ) ");
+              }
+            }
 
 
             ?>
@@ -257,7 +256,7 @@ function addBid(){
 
 
     <?php include("baseFooter.php"); ?>
-    
+
   </body>
 
 
--- dbCoursework/dashboard/search_result_page.php
+++ dbCoursework/dashboard/search_result_page.php
@@ -8,15 +8,51 @@
   <?php #include('baseBody.php'); ?>
 
   <?php
-  $searchTerm = "car"; # PLACEHOLDER SEARCH TERM UNTIL SEARCHBAR WORKS
-  $searchTerm = strtolower($searchTerm); # Convert to lowercase
+    $searchTerm = "great"; # PLACEHOLDER SEARCH TERM UNTIL SEARCHBAR WORKS
+    $searchTerm = strtolower($searchTerm); # Convert to lowercase
 
-  echo($searchTerm); # For testing
+    echo($searchTerm); # FOR TESTING
 
-  $query_result = $conn->query("SELECT itemID, title, description, photo, endDate, startPrice FROM items i WHERE i.title LIKE '%$searchTerm%' OR i.description LIKE '%$searchTerm%' GROUP BY i.itemID ORDER BY i.itemViewCount DESC LIMIT 100");
-  $result_table = $query_result->fetch(); # Fetch the query result into an array
+    $query_result = $conn->query("SELECT itemID, title, description, photo, endDate, startPrice
+                                FROM items i
+                                WHERE LOWER(i.title) LIKE '%$searchTerm%'
+                                    OR LOWER(i.description) LIKE '%$searchTerm%'
+                                GROUP BY i.itemID
+                                ORDER BY i.itemViewCount DESC");
 
-  var_dump($result_table); # For testing
+    $count_result = $conn->query("SELECT COUNT(itemID) FROM (SELECT itemID
+                          FROM items i
+                          WHERE LOWER(i.title) LIKE '%$searchTerm%'
+                              OR LOWER(i.description) LIKE '%$searchTerm%'
+                          GROUP BY i.itemID
+                          ORDER BY i.itemViewCount DESC) AS count");
+
+    $temp = $count_result -> fetch();
+    $number_of_results = $temp['COUNT(itemID)'];
+
+    # Iterate through search results:
+    for($itemCount = 0; $itemCount < $number_of_results; $itemCount++) {
+        $result_table = $query_result->fetch(); # Fetch the query result into an array
+        var_dump($result_table); # FOR TESTING
+
+        # Variables:
+        $itemID = $result_table['itemID'];
+        $title = $result_table['title'];
+        $description = $result_table['description'];
+        $photo = $result_table['photo'];
+        $date = $result_table['endDate'];
+        $startPrice = $result_table['startPrice'];
+
+        $bid_query = $conn->query("SELECT bidAmount, bidDate FROM bids WHERE itemID = $itemID ORDER BY bidAmount LIMIT 1");
+
+        $bids_on_item = $bid_query -> fetch();
+        var_dump($bids_on_item); # FOR TESTING
+
+        # Variables:
+        $currentPrice = $bids_on_item['bidAmount'];
+        $lastBid = $bids_on_item['bidDate'];
+
+    }
   ?>
 
 </body>

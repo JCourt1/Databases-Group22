762e23674d5b6544aa0cc7c248d540d1eaa93697
Mark <markbackhouse@live.co.uk>
2018-03-08 17:54:06 +0000

    Deleting old code on popular items page

--- dbCoursework/dashboard/commonElements/browsePopularItems.php
+++ dbCoursework/dashboard/commonElements/browsePopularItems.php
@@ -8,61 +8,51 @@ if(isset($_SESSION['user_ID'])){
 } else{
     $buyerID = NULL;
 }
-
-
 ?>
 
-    <div class="row placeholders">
-        <?php
+<div class="row placeholders">
+    <?php
 
-        $querry_result = $conn->query("SELECT itemID, title, description, photo, endDate, startPrice FROM items WHERE endDate > NOW() ORDER BY itemViewCount DESC LIMIT 4");
-        $count_result = $conn->query("SELECT COUNT(itemID) FROM ( SELECT itemID FROM items WHERE endDate > NOW()   ORDER BY itemViewCount DESC LIMIT 4 ) AS count");
-        $data3 = $count_result->fetch();
-        $rowcount = $data3['COUNT(itemID)'];
+    $statement = $conn->prepare("SELECT itemID, title, description, photo, endDate, startPrice
+                                FROM items
+                                WHERE endDate > NOW()
+                                ORDER BY itemViewCount DESC LIMIT 8");
+    $statement->execute();
+    $res = $statement->fetchAll();
 
-        // Test for now:///////////////////////////////////
-        $statement = $conn->prepare("SELECT itemID, title, description, photo, endDate, startPrice
-                                    FROM items
-                                    WHERE endDate > NOW()
-                                    ORDER BY itemViewCount DESC LIMIT 8");
-        $statement->execute();
-        $res = $statement->fetchAll();
+    $rownumber = 0;
+    // Iterate through the 8 most popular items:
+    foreach ($res as $searchResult) {
 
-        $rownumber = 0;
-        // Iterate through the 8 most popular items:
-        foreach ($res as $searchResult) {
+        // Item information:
+        $itemID = $searchResult['itemID'];
+        $title = $searchResult['title'];
+        $photo = $searchResult['photo'];
+        $description = $searchResult['description'];
+        $startPrice = $searchResult['startPrice'];
 
-            // Item information:
-            $itemID = $searchResult['itemID'];
-            $title = $searchResult['title'];
-            $photo = $searchResult['photo'];
-            $description = $searchResult['description'];
-            $startPrice = $searchResult['startPrice'];
+        // Bid information:
+        $bid_query = $conn->prepare("SELECT buyerID, bidAmount, bidDate
+                                    FROM bids
+                                    WHERE itemID = " .$itemID. "
+                                    ORDER BY bidAmount DESC LIMIT 1");
+        $bid_query->execute();
+        $bid = $bid_query->fetch();
+        $currentPrice = $bid['bidAmount'];
+        $lastBid = $bid['bidDate'];
 
-            // Bid information:
-            $bid_query = $conn->prepare("SELECT buyerID, bidAmount, bidDate
-                                        FROM bids
-                                        WHERE itemID = " .$itemID. "
-                                        ORDER BY bidAmount DESC LIMIT 1");
-            $bid_query->execute();
-            $bid = $bid_query->fetch();
-            $currentPrice = $bid['bidAmount'];
-            $lastBid = $bid['bidDate'];
+        $current_date =  new DateTime();
 
-            $current_date =  new DateTime();
+        $bid_end_date =  new DateTime($searchResult['endDate']);
+        $interval = $current_date->diff($bid_end_date);
+        $elapsed = $interval->format('%y y %m m %a d %h h %i min %s s');
 
-            $bid_end_date =  new DateTime($searchResult['endDate']);
-            $interval = $current_date->diff($bid_end_date);
-            $elapsed = $interval->format('%y y %m m %a d %h h %i min %s s');
+        // MODAL:
+        include $_SERVER['DOCUMENT_ROOT']."$siteroot/dashboard/commonElements/itemModal.php";
 
-            // MODAL:
-            include $_SERVER['DOCUMENT_ROOT']."$siteroot/dashboard/commonElements/itemModal.php";
+        $rownumber += 1;
+    }
 
-            $rownumber += 1;
-        }
+    ?>
 
-        ?>
-
-    </div>
-
-</div>
+</div>  

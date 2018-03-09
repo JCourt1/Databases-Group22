21f038cf6680e5e2022d54bfdcf0f7d8e2791b0d
Dimitris Selalmazidis <ucabela@ucl.ac.uk>
2018-03-09 15:24:02 +0000

    Profile

--- dbCoursework/profile/profile_details.php
+++ dbCoursework/profile/profile_details.php
@@ -36,42 +36,37 @@ $data=$res->fetch();
       
       <!-- edit form column -->
       <div class="col-md-9 col-md-offset-2 personal-info">
-        <div class="alert alert-info alert-dismissable">
-          <a class="panel-close close" data-dismiss="alert">×</a> 
-          <i class="fa fa-coffee"></i>
-          This is an <strong>.alert</strong>. Use this to show important messages to the user.
-        </div>
         <h3>Personal info</h3>
         
         <form class="form-horizontal" role="form">
           <div class="form-group">
             <label class="col-lg-3 control-label">First name:</label>
             <div class="col-lg-8">
-              <input class="form-control" name="firstName" placeholder="Please enter your first name" type="text">
+              <input class="form-control" name="firstName" value="<?php if(isset($data['firstName'])){echo $data['firstName'];};  ?>" placeholder="Please enter your first name" type="text">
             </div>
           </div>
           <div class="form-group">
             <label class="col-lg-3 control-label">Last name:</label>
             <div class="col-lg-8">
-              <input class="form-control" name="LastName" placeholder="Please enter your last name" type="text">
+              <input class="form-control" name="LastName" value="<?php if(isset($data['lastName'])){echo $data['lastName'];};?>" placeholder="Please enter your last name" type="text">
             </div>
           </div>
           <div class="form-group">
             <label class="col-lg-3 control-label">Phone number:</label>
             <div class="col-lg-8">
-              <input class="form-control" name="phone" placeholder="Please enter your phone number" type="text">
+              <input class="form-control" name="phone" value="<?php if(isset($data['phoneNumber'])){echo $data['phoneNumber'];};?>" placeholder="Please enter your phone number" type="text">
             </div>
           </div>
           <div class="form-group">
             <label class="col-lg-3 control-label">Company:</label>
             <div class="col-lg-8">
-              <input class="form-control" placeholder="Please enter your company name" name="company" value="" type="text">
+              <input class="form-control" name="company" value="<?php if(isset($data['companyName'])){echo $data['companyName'];};?>" placeholder="Please enter your company name"  type="text">
             </div>
           </div>
           <div class="form-group">
             <label class="col-lg-3 control-label">Profile Picture:</label>
             <div class="col-lg-8">
-              <input class="form-control" name="picture" placeholder="Please enter your profile picture link" type="text">
+              <input class="form-control" name="picture" value="<?php if(isset($data['profilePic'])){echo $data['profilePic'];};?>"  placeholder="Please enter your profile picture link"  type="text">
             </div>
           </div>
           <div class="form-group">
@@ -81,21 +76,52 @@ $data=$res->fetch();
             </div>
           </div>
           <div class="form-group">
+            <label class="col-lg-3 control-label">Street:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="street" value="<?php if(isset($data['streetName'])){echo $data['streetName'];};?>"  placeholder="Please enter your address" type="text">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Building Number:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="buildingNumber" value="<?php if(isset($data['buildingNumber'])){echo $data['buildingNumber'];};?>"  placeholder="Please enter your building number" type="text">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">City:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="city" value="<?php if(isset($data['cityName'])){echo $data['cityName'];};?>" type="text">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Country:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="country" value="<?php if(isset($data['countryName'])){echo $data['countryName'];};?>" type="text">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Post code:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="postCode" value="<?php if(isset($data['postCode'])){echo $data['postCode'];};?>" type="text">
+            </div>
+          </div>
+          
+          <div class="form-group">
             <label class="col-md-3 control-label">Username:</label>
             <div class="col-md-8">
-              <input class="form-control" value="<?php echo $data['username'];  ?>" type="text">
+              <input class="form-control" name="username" value="<?php echo $data['username'];  ?>" type="text">
             </div>
           </div>
           <div class="form-group">
             <label class="col-md-3 control-label">Password:</label>
             <div class="col-md-8">
-              <input class="form-control" placeholder="Enter your new password" type="password">
+              <input class="form-control" name="psw" placeholder="Enter your new password" type="password">
             </div>
           </div>
           <div class="form-group">
             <label class="col-md-3 control-label">Confirm password:</label>
             <div class="col-md-8">
-              <input class="form-control" placeholder="Confirm your new password"  type="password">
+              <input class="form-control" name="psw-confirm" placeholder="Confirm your new password"  type="password">
             </div>
           </div>
           <div class="form-group">

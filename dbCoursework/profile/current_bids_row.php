b98a85fd55a0c75a5004b502e9eb83d40f549a2a
Mark <markbackhouse@live.co.uk>
2018-03-09 13:13:34 +0000

    Added a "My Bids" Page

---
+++ dbCoursework/profile/current_bids_row.php
@@ -0,0 +1,38 @@
+<?php
+
+$tableRow = '
+<tr scope="row">
+
+        <td>
+            '.$row["title"].'
+        </td>
+
+        <td>
+            '.$category["categoryName"].'
+        </td>
+
+        <td>
+            £'.$row["bidAmount"].'
+        </td>
+
+        <td>
+            '.$row["bidDate"].'
+        </td>
+
+        <td>
+            '.$bidWinning.'
+        </td>
+
+        <td>
+            <a href="'.$siteroot.'browse/auctionRooms.php?itemID='.$row['itemID'].'">View in auction room</a>
+        </td>
+
+        <td>
+            <a href="'.$siteroot.'removeBidMaster.php?bidID='.$row['bidID'].'">Remove Bid</a>
+        </td>
+
+</tr>
+';
+
+echo $tableRow;
+?>
